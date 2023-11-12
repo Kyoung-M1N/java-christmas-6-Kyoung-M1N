317aeb81e58a4d1d9714601a686bd3642bdc7b3d
kymin <park48664866@gmail.com>
2023-11-12 15:28:55 +0900

    refactor: 출력메시지 상수처리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -7,13 +7,16 @@ import christmas.model.Menu;
 import java.util.Map;
 
 public class InputView {
+    private static final String INPUT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String INPUT_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
     public static int readDate() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        System.out.println(INPUT_DATE_MESSAGE);
         return DateValidator.validate(Console.readLine());
     }
 
     public static Map<Menu, Integer> order() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println(INPUT_MENU_MESSAGE);
         return MenuValidator.validate(Console.readLine());
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,14 +7,18 @@ import java.text.DecimalFormat;
 import java.util.Map;
 
 public class OutputView {
+    private static final String INTRO_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n";
     private static final String MENU_PRINT = "%s %d개\n";
+    private static final String PRICE = "%s원\n";
+    private static final DecimalFormat formatter = new DecimalFormat("#,###,###");
 
     public static void printIntroMessage() {
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println(INTRO_MESSAGE);
     }
 
     public static void printMenu(Map<Menu, Integer> order, int date) {
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n", date);
+        System.out.printf(PREVIEW_MESSAGE, date);
         System.out.println("<주문 메뉴>");
 
         for (Menu menu : order.keySet()) {
@@ -24,7 +28,6 @@ public class OutputView {
 
     public static int printPrice(Map<Menu, Integer> order) {
         System.out.println("\n<할인 전 총주문 금액>");
-        DecimalFormat formatter = new DecimalFormat("#,###,###");
         int price = 0;
         for (Menu menu : order.keySet()) {
             price += (menu.getPrice() * order.get(menu));
@@ -43,33 +46,30 @@ public class OutputView {
     }
 
     public static void printBenefits(Map<Event, Integer> benefit) {
-        DecimalFormat formatter = new DecimalFormat("###,###");
         System.out.println("\n<혜택 내역>");
         if (benefit.isEmpty()) {
-            System.out.println("없음\n");
+            System.out.println("없음");
         } else if (!benefit.isEmpty()) {
             for (Event event : benefit.keySet()) {
-                System.out.printf("%s: -%s원\n", event.getName(), formatter.format(benefit.get(event)));
+                System.out.printf("%s: -" + PRICE, event.getName(), formatter.format(benefit.get(event)));
             }
         }
 
     }
 
     public static int printBenefitPrice(Map<Event, Integer> benefit) {
-        DecimalFormat formatter = new DecimalFormat("###,###");
         int benefitPrice = 0;
         System.out.println("\n<총혜택 금액>");
         for (Event event : benefit.keySet()) {
             benefitPrice += benefit.get(event);
         }
-        System.out.printf("%s원\n", formatter.format(benefitPrice));
+        System.out.printf(PRICE, formatter.format(benefitPrice));
         return benefitPrice;
     }
 
     public static void printPayment(int price, int discount) {
-        DecimalFormat formatter = new DecimalFormat("###,###");
         System.out.println("\n<할인 후 예상 결제 금액>");
-        System.out.printf("%s원\n", formatter.format(price - discount));
+        System.out.printf(PRICE, formatter.format(price - discount));
     }
 
     public static void printBadge(int discount) {
